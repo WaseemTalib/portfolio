192b92a960e790f120ba3f72418f8927a8f8bff6
WaseemTalib <waseemltalib@gmail.com>
2024-01-13 02:13:48 +0500

    changes

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -16,8 +16,9 @@ const Navbar = () => {
                     <ul className="menu">
 
                         <li><a href="#about">about</a></li>
-                        
+
                         <li><a href="#skills">skills</a></li>
+                        
                         <li><a href="#work">my Projects</a></li>
                         <label htmlFor="chk" className="hide-menu-btn">
                             <i className="fa fa-times" aria-hidden="true"></i>
